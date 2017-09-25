d38215a56c74c33d6d93dd0ce172fc9e731401e3
Avatar <prachaya.su@gmail.com>
2017-09-25 12:04:42 +0700

    add test call

--- src/KSequencing.php
+++ src/KSequencing.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* namespace Buonzz\Template; */
 /* require_once dirname(__FILE__) . 'k-sequencing/'; */
 
@@ -10,7 +10,7 @@ require_once dirname(__FILE__).'/k-sequencing/models/imagePhotoTags.php';
 
 
 // post test
-$params = array('instruction' => 'face', 'categories' => 'p h p', 'data' => 'https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg', 'postback_url' => 'https://kseq.datawow.io/', 'multiple' => true);
+$params = array('instruction' => 'face', 'categories' => 'p h p', 'data' => 'https://s3-ap-southeast-1.amazonaws.com/kiyo-images/2016/10/10/57fada626288cf0a692b6070.jpg', 'postback_url' => 'https://kseq.datawow.io/', 'multiple' => true);
 $s = ImageChoices::create('xkA4eW3ZPHD17Pj81xBY5V7Q', $params);
 
 
@@ -19,4 +19,49 @@ echo('--------------------------------');
 
 // get test
 $s = ImageChoices::get('xkA4eW3ZPHD17Pj81xBY5V7Q');
+print_r(json_decode($s));
+
+
+echo('--------------------------------');
+echo('--------------------------------');
+// post test
+$params = array('instruction' => 'face', 'data' => 'https://s3-ap-southeast-1.amazonaws.com/kiyo-images/2016/10/10/57fada626288cf0a692b6070.jpg', 'postback_url' => 'https://kseq.datawow.io/', 'multiple' => true);
+$s = ImageClosedQuestions::create('HK7eB6PHztfMZmhoeXPrRAHk', $params);
+
+
+print_r(json_decode($s));
+echo('--------------------------------');
+
+// get test
+$s = ImageClosedQuestions::get('HK7eB6PHztfMZmhoeXPrRAHk');
+print_r(json_decode($s));
+
+echo('--------------------------------');
+echo('--------------------------------');
+
+// post test
+$params = array('instruction' => 'face', 'data' => 'https://s3-ap-southeast-1.amazonaws.com/kiyo-images/2016/10/10/57fada626288cf0a692b6070.jpg', 'postback_url' => 'https://kseq.datawow.io/', 'multiple' => true);
+$s = ImageMessages::create('3paHN4Jx2EeEERCHupPMkHzs', $params);
+
+
+print_r(json_decode($s));
+echo('--------------------------------');
+
+// get test
+$s = ImageMessages::get('3paHN4Jx2EeEERCHupPMkHzs');
+print_r(json_decode($s));
+
+
+echo('--------------------------------');
+echo('--------------------------------');
+// post test
+$params = array('instruction' => 'face', 'data' => 'https://s3-ap-southeast-1.amazonaws.com/kiyo-images/2016/10/10/57fada626288cf0a692b6070.jpg', 'postback_url' => 'https://kseq.datawow.io/', 'multiple' => true);
+$s = ImagePhotoTags::create('ZwuaoaUHMYMHCbZh6TKUMpg7', $params);
+
+
+print_r(json_decode($s));
+echo('--------------------------------');
+
+// get test
+$s = ImagePhotoTags::get('ZwuaoaUHMYMHCbZh6TKUMpg7');
 print_r(json_decode($s));
