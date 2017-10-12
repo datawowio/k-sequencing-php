ba61440210abc24e526165927376f97c1ffd61c9
Avatar <prachaya.su@gmail.com>
2017-10-12 14:20:18 +0700

    add predicton api

--- src/KSequencing.php
+++ src/KSequencing.php
@@ -4,3 +4,4 @@ require_once dirname(__FILE__).'/handle/models/imageChoices.php';
 require_once dirname(__FILE__).'/handle/models/imageClosedQuestions.php';
 require_once dirname(__FILE__).'/handle/models/imageMessages.php';
 require_once dirname(__FILE__).'/handle/models/imagePhotoTags.php';
+require_once dirname(__FILE__).'/handle/models/predictions.php';
---
+++ src/handle/models/predictions.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once dirname(__FILE__).'/../connections/Connector.php';
+
+
+class Predictions extends Connector
+{
+  const url_endpoint = 'prime/predictions';
+  
+  public static function create($token, $params = array())
+  {
+    return parent::create_image(get_class(), self::url_endpoint, $token, $params);
+  }
+
+  public static function get($token, $params = array())
+  {
+    $params = array('page' => 0, 'paerPage' => 20);
+    return parent::get_image(get_class(), self::url_endpoint, $token, $params);
+  }
+
+  
+  public static function get_id($token, $params = array())
+  {
+    return parent::get_image(get_class(), self::url_endpoint + "/" + $params["id"], $token, $params);
+  }
+
+} 
