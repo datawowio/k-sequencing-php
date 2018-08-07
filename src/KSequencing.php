87f903d58f58bd32677dfca3d9e16ac3d37a07f3
Avatar <prachaya.su@gmail.com>
2018-08-07 11:25:06 +0700

    fix path

--- src/KSequencing.php
+++ src/KSequencing.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once dirname(__FILE__).'/handle/models/imageChoices.php';
-require_once dirname(__FILE__).'/handle/models/imageClosedQuestions.php';
-require_once dirname(__FILE__).'/handle/models/imageMessages.php';
-require_once dirname(__FILE__).'/handle/models/imagePhotoTags.php';
-require_once dirname(__FILE__).'/handle/models/predictions.php';
+require_once dirname(__FILE__).'/handle/models/images/imageChoices.php';
+require_once dirname(__FILE__).'/handle/models/images/imageClosedQuestions.php';
+require_once dirname(__FILE__).'/handle/models/images/imageMessages.php';
+require_once dirname(__FILE__).'/handle/models/images/imagePhotoTags.php';
+require_once dirname(__FILE__).'/handle/models/predictions/predictors.php';
