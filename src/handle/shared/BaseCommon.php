bd1d565e8622f12cf539e44682233c196457b24f
Avatar <prachaya.su@gmail.com>
2018-08-08 11:18:47 +0700

    made support php <= 5.4

--- src/handle/shared/BaseCommon.php
+++ src/handle/shared/BaseCommon.php
@@ -2,24 +2,24 @@
 
 function get_path($type, $model)
 {
-    $url = ["images"=> [
+    $url = array("images"=> array(
            "closed_questions"=> "images/closed_questions",
            "messages"=> "images/messages",
            "choices"=> "images/choices",
            "photo_tag"=> "images/photo_tags",
-           "find"=> "projects/images/" ],
-         ["videos" => [
-           "closed_questions" => "videos/closed_questions"] ],
-         ["texts"=> [
+           "find"=> "projects/images/" ),
+         array("videos" => array(
+           "closed_questions" => "videos/closed_questions") ),
+         array("texts"=> array(
            "categories" => "v1/text/text_categories",
            "closed_questions"=> "v1/text/text_closed_questions",
            "conversations"=> "v1/text/text_conversations"
-         ]  ],
-         ["ai" => [
+         )  ),
+         array("ai" => array(
           "predictor" => "prime/predictions",
           "find" => "projects/images/"
-         ] ]
-      ];
+         ) )
+      );
 
     return $url[$type][$model];
 }
