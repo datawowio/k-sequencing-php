7d386a40f2f668e99f4d9e791e7c67873fbf94fb
Avatar <prachaya.su@gmail.com>
2018-08-09 09:30:36 +0700

    add video model

---
+++ src/handle/models/videos/videoClassify.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once dirname(__FILE__).'/../../shared/BaseCommon.php';
+require_once dirname(__FILE__).'/../../connections/Connector.php';
+
+class VideoClassify extends Connector
+{
+    public static function create($token, $params = array())
+    {
+        return parent::create_data(get_class(), base_url('videos').get_path('videos', 'closed_questions'), $token, $params);
+    }
+
+    public static function gets($token, $params = array())
+    {
+        $params = array('page' => 0, 'paerPage' => 20);
+
+        return parent::retrive_list(get_class(), base_url('videos').get_path('videos', 'closed_questions'), $token, $params);
+    }
+
+    public static function find_id($token, $id)
+    {
+        return parent::retrive_once(get_class(), base_url('videos').get_path('videos', 'closed_questions'), $token, $id);
+    }
+}
